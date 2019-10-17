5ca8d15c50677ad4bba56b77c0a54f6bee614a63
Anita Kemp, Jacqueline Remmel and Sofia Giordano <sofia@>
2019-10-17 14:46:30 -0700

    Fix binding of restaurantId property during creation of cuisine object.

--- Best/Controllers/CuisineController.cs
+++ Best/Controllers/CuisineController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace Best.Controllers
 {
@@ -15,31 +16,39 @@ namespace Best.Controllers
             _db = db;
         }
 
-        public ActionResult Index()
-        {
-            List<Cuisine> model = _db.Cuisines.Include(m => m.Restaurant).ToList();
-            // ViewBag.RestaurantId = id;
-            return View(model);
-        }
+        //GOT RID OF CUISINE INDEX VIEW
+        // public ActionResult Index()
+        // {
+        //     List<Cuisine> model = _db.Cuisines.Include(m => m.Restaurant).ToList();
+        //     // ViewBag.RestaurantId = id;
+        //     return View(model);
+        // }
 
+        //Cuisine Create: displays a form to make a new cuisine
         public ActionResult Create(int id)
         {
+            //find the restaurant from DB
+            //pass in as model
             ViewBag.RestaurantId = id;
             return View();
         }
 
+        //Cuisine Create Post: Accepts new cuisine object from form, adds it to database, and redirects to Restaurant Details page
         [HttpPost]
-        public ActionResult Create(int id, Cuisine cuisine)
+        public ActionResult Create(Cuisine cuisines)
         {
-            _db.Cuisines.Add(cuisine);
+            // Console.WriteLine(">>>>>ID passed in to Cuisine Controller Create Method: " + id);
+            _db.Cuisines.Add(cuisines);
             _db.SaveChanges();
-            return RedirectToAction("Details", "Restaurant", id);
+            return RedirectToAction("Details", "Restaurant", new { id = cuisines.RestaurantId });
         }
 
+        //Displays details for a cuisine
         public ActionResult Details(int id)
         {
             Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(m => m.CuisineId == id);
             return View(thisCuisine);
         }
+
     }
 }
--- Best/Controllers/RestaurantController.cs
+++ Best/Controllers/RestaurantController.cs
@@ -16,17 +16,20 @@ namespace Best.Controllers
             _db = db;
         }
 
+        //Restaurant Index: displays list of restaurants in the database
         public ActionResult Index()
         {
             List<Restaurant> model = _db.Restaurants.ToList();
             return View(model);
         }
 
+        //Restaurant Create: displays a form to make a new restaurant
         public ActionResult Create()
         {
             return View();
         }
 
+        //Restaurant Create Post: Accepts new restaurant object, adds it to database, and redirects to Restaurant Index
         [HttpPost]
         public ActionResult Create(Restaurant restaurants) //the parameter name "restaurants" refers to the actual table in the database, so it must have the same name as the table otherwise it will yell at you.
         {
@@ -35,9 +38,10 @@ namespace Best.Controllers
             return RedirectToAction("Index");
         }
 
+        //Restaurant Details: displays list of cuisines for a particular restaurant ("int id" refers to the restaurant's ID)
         public ActionResult Details(int id)
         {
-            Console.WriteLine(">>>>>ID passed in to Details route of Restaurant Controller: " + id);
+            Console.WriteLine(">>>>>ID passed in to Restaurant Controller Details Method: " + id);
             Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(rest=> rest.RestaurantId == id);
             return View(thisRestaurant);
         }
